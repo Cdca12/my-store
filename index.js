34a56aa50b43b122e09ef7b3fb73d6e5afac85c1
Carlos Contreras <cdca12mx@gmail.com>
2022-08-11 01:04:08 -0600

    Added new endpoint test and endpoint to return products

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ app.get('/', (req, res) => {
   res.send('Hola mundo');
 });
 
+app.get('/new-endpoint', (req, res) => {
+  res.send('Hola soy un nuevo endpoint');
+});
+
+app.get('/products', (req, res) => {
+  res.json({
+    id: 1,
+    name: 'Product 1',
+    price: 1000
+  });
+});
+
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
 });
