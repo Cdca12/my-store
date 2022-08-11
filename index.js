18f42d9647d0ceaee22b574543a29f8e511f2e5f
Carlos Contreras <cdca12mx@gmail.com>
2022-08-11 00:58:55 -0600

    Created server in Express.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('My Store');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hola mundo');
+});
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
