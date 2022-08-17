37fe2807bcfc357586984a4946cd3983a7c853d4
Carlos Contreras <cdca12mx@gmail.com>
2022-08-17 00:21:58 -0600

    GET with params

--- index.js
+++ index.js
@@ -10,12 +10,49 @@ app.get('/new-endpoint', (req, res) => {
   res.send('Hola soy un nuevo endpoint');
 });
 
-app.get('/products', (req, res) => {
-  res.json({
+const products = [
+  {
     id: 1,
     name: 'Product 1',
-    price: 1000
-  });
+    price: 1000,
+    category: {
+      id: 1,
+      name: 'Category 1'
+    }
+  },
+  {
+    id: 2,
+    name: 'Product 2',
+    price: 2000,
+    category: {
+      id: 2,
+      name: 'Category 2'
+    }
+  },
+  {
+    id: 3,
+    name: 'Product 3',
+    price: 3000,
+    category: {
+      id: 1,
+      name: 'Category 1'
+    }
+  }
+];
+
+app.get('/products', (req, res) => {
+  res.json(products);
+});
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
+  let product = products.find(product => product.id.toString() === id);
+  res.json(product);
+});
+
+app.get('/categories/:categoryId/products/:price', (req, res) => {
+  const { categoryId, price } = req.params;
+  res.json(products.filter(product => product.category.id == categoryId && product.price >= price));
 });
 
 app.listen(port, () => {
