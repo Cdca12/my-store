4093ca4af9d515ec3511bc126ca88f9b56604680
Carlos Contreras <cdca12mx@gmail.com>
2022-08-18 01:23:34 -0600

    Added categoriesRouter

---
+++ routes/categoriesRouter.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const faker = require('faker');
+
+const router = express.Router();
+
+const categories = [];
+
+function generateCategories() {
+  for (let i = 0; i < 100; i++) {
+    categories.push({
+      id: i + 1,
+      name: faker.commerce.productAdjective(),
+      product: {
+        id: Math.floor(Math.random() * 100)
+      }
+    });
+  }
+}
+
+// Inicializar lista de categories
+generateCategories();
+
+// Endpoints
+router.get('/', (req, res) => {
+  const { size } = req.query;
+  const limit = size || 10;
+  res.json(categories.slice(0, limit));
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  let category = categories.find(category => category.id == id);
+  res.json(category);
+});
+
+router.get('/products/:productId', (req, res) => {
+  const { productId } = req.params;
+  let categoriesList = categories.filter(category => category.product.id == productId);
+  res.json(categoriesList);
+});
+
+
+module.exports = router;
