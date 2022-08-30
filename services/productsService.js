d60a1483408fd9c0b91ba7baed6d1421a6aa5665
Carlos Contreras <cdca12mx@gmail.com>
2022-08-30 01:07:47 -0600

    Added validations Product not found

--- services/productsService.js
+++ services/productsService.js
@@ -16,7 +16,6 @@ class ProductsService {
         price: parseInt(faker.commerce.price(), 10),
         image: faker.image.imageUrl()
       });
-
     }
   }
 
@@ -31,21 +30,28 @@ class ProductsService {
   create(product) {
     let lastProduct = this.products[this.products.length - 1];
     let newId = lastProduct.id + 1;
-    this.products.push({
+    let newProduct = {
       id: newId,
       ...product
-    });
-    return this.products.find(product => product.id == newId);
+    }
+    this.products.push(newProduct);
+    return newProduct;
   }
 
   update(id, product) {
     let index = this.products.findIndex(product => product.id == id);
+    if (index < 0) {
+      throw new Error('Product not found');
+    }
     this.products[index] = product;
     return product;
   }
 
   delete(id) {
     let index = this.products.findIndex(product => product.id == id);
+    if (index < 0) {
+      throw new Error('Product not found');
+    }
     return this.products.splice(index, 1);
   }
 
